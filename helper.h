575180f0108cb0d4b6a66fe05a23720be22d2406
mansbota <74121311+mansbota@users.noreply.github.com>
2021-11-30 12:13:54 +0100

    Delete helper.h

--- helper.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <ntifs.h>
-#include <windef.h>
-
-NTSTATUS CreateStandardSCAndACL(OUT PSECURITY_DESCRIPTOR* SecurityDescriptor, OUT PACL* Acl);
-NTSTATUS GrantAccess(HANDLE hSection, IN PACL StandardAcl);
